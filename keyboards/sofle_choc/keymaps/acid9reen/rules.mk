3e14a1fd0deb7e4711523d35451d2e3e036ecd2f
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2023-11-05 13:34:48 +0300

    Some improvements
    * Decrease firmware size via disabling most of rgb animations
    *Remove configuration duplication (with info.json)

--- keyboards/sofle_choc/keymaps/acid9reen/rules.mk
+++ keyboards/sofle_choc/keymaps/acid9reen/rules.mk
@@ -1,6 +1,10 @@
 ENCODER_MAP_ENABLE = yes
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = ws2812
-OLED_ENABLE = yes
-LTO_ENABLE = yes
 VIA_ENABLE = yes
+
+# Override options defined in info.json
+BOOTMAGIC_ENABLE = no
+MOUSEKEY_ENABLE = no
+
+# Disable defaults to reduce firmware size
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
